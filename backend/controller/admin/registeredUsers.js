2e16037d3f6c595dd45a676e95c76885ebb89916
RichDotCom <emmanueltokyo24@gmail.com>
2024-07-29 16:12:16 +0100

    made a change to the backend

--- backend/controller/admin/registeredUsers.js
+++ backend/controller/admin/registeredUsers.js
@@ -6,10 +6,10 @@ export const registedrUsers = async (req, res)=>{
 
         if(!user) return res.status(400).json({message: "There is no registered person!"})
 
-        return res.status(200).json({user})
+        return res.status(200).json(user)
 
     }catch(e){
         console.log(e.message)
         return res.status(500).json({message: "Sever error"})
     }
-}
+} 
